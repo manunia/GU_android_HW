ffe5dec0990c595010fa5ad087f80e59bfee5bf4
manunia <timoshenkomaria@mail.ru>
2020-04-07 18:50:31 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/gu_android_hw/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.gu_android_hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
