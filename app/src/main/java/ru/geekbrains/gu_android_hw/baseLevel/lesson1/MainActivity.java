4b78ecdae007b08a44ff2e520cbf2aa61b9a6129
manunia <timoshenkomaria@mail.ru>
2020-04-09 20:38:47 +0300

    экран, на котором отображаются названия городов и температура в градусах цельсия

--- app/src/main/java/ru/geekbrains/gu_android_hw/baseLevel/lesson1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/gu_android_hw/baseLevel/lesson1/MainActivity.java
@@ -1,8 +1,10 @@
-package ru.geekbrains.gu_android_hw;
+package ru.geekbrains.gu_android_hw.baseLevel.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import ru.geekbrains.gu_android_hw.R;
 
 public class MainActivity extends AppCompatActivity {
 
