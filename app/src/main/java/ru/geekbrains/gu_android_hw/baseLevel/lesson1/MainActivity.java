ecc856eabd067851bb2e4fbbd513ba09e8032518
manunia <timoshenkomaria@mail.ru>
2020-04-11 19:59:35 +0300

    добавили экран выбора города и обработку нажатия на кнопку.

--- app/src/main/java/ru/geekbrains/gu_android_hw/baseLevel/lesson1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/gu_android_hw/baseLevel/lesson1/MainActivity.java
@@ -3,14 +3,38 @@ package ru.geekbrains.gu_android_hw.baseLevel.lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import ru.geekbrains.gu_android_hw.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button submitButton;
+    EditText textCity;
+    String enteredText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.select_activity);
+
+        submitButton = findViewById(R.id.submit_buton);
+        textCity = findViewById(R.id.edit_city);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enteredText = textCity.getText().toString();
+                if (enteredText.length() == 0) {
+                    Toast.makeText(MainActivity.this, "Invalid enter", Toast.LENGTH_SHORT).show();
+                } else {
+                    setContentView(R.layout.activity_main);
+                }
+            }
+        });
     }
 }
