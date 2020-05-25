27ec134dfd2dd30ab90b33e4b3860977f8ff0aff
manunia <timoshenkomaria@mail.ru>
2020-05-25 17:04:39 +0300

    убран кастомный элемент - градусник

--- app/src/main/java/ru/geekbrains/gu_android_hw/baseLevel/lesson1/ui/TermometrView.java
+++ app/src/main/java/ru/geekbrains/gu_android_hw/baseLevel/lesson1/ui/TermometrView.java
@@ -32,7 +32,7 @@ public class TermometrView extends View {
     // Отступ элементов
     private final static int padding = 50;
     // Скругление углов батареи
-    private final static int round = 25;
+    private final static int round = 45;
     // Ширина головы батареи
     private final static int headWidth = 40;
 
